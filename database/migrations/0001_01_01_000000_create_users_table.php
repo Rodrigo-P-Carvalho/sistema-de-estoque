ce46a1d2322580867f9c00854f99e824fb5387de
Rodrigo-P-Carvalho <rodrigopc6161@gmail.com>
2026-05-19 15:53:08 -0300

    Usando migration de users agora

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->foreignId('perfil_id')->constrained('perfis')->onDelete('cascade');
             $table->rememberToken();
             $table->timestamps();
         });
